6b872014080a2c3f18e0b02bf1116464cf248255
bonny-art <svitlana.otenko@gmail.com>
2025-11-12 03:44:52 +0200

    Minor fix

--- helpers/validateBody.js
+++ helpers/validateBody.js
@@ -5,7 +5,7 @@ const validateBody = (schema) => {
     const { error } = schema.validate(req.body, { abortEarly: false });
 
     if (error) {
-      next(HttpError(400, error.message));
+      return next(HttpError(400, error.message));
     }
     next();
   };
